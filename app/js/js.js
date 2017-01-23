fd94a95ab4928d02626e48effeb924267569e044
Hayder <hayder@etereo.io>
2017-01-23 12:16:49 +0100

    Bio tests

--- app/js/js.js
+++ app/js/js.js
@@ -14,3 +14,18 @@
   $('.menu a').click(toggleMenu);
 
 })();
+
+$(document).ready(function() {
+  $('.bio-opener-hayder').on('click', function() {
+   $('.bio-hayder').toggleClass('show');
+   $('body').toggleClass('menu-open');
+  });
+  $('.bio-opener-anthanh').on('click', function() {
+   $('.bio-anthanh').toggleClass('show');
+   $('body').toggleClass('menu-open');
+  });
+  $('.people-bio .bio-closer').on('click', function() {
+   $('.people-bio').removeClass('show');
+   $('body').removeClass('menu-open');
+  });
+});
