e7a5c5ed6063f3bd5884f69181ac2af6caafb5d5
Anthanh Pham Trinh <antai.pt@gmail.com>
2017-01-24 01:17:12 +0100

    parallax added scroll reveal added z-index changed menu fade updated

--- app/js/js.js
+++ app/js/js.js
@@ -1,37 +1,65 @@
 'use strict';
+/* jshint newcap:false */
+/* globals Rellax, ScrollReveal, Waypoint */
 
 (function() {
 
-  var menuToggle = $('.menu-toggle');
-  var body = $('body');
+  $(document).ready(function() {
 
-  function toggleMenu() {
-    menuToggle.toggleClass('active');
-    body.toggleClass('menu-open');
-  }
+    var menuToggle = $('.menu-toggle');
+    var body = $('body');
 
-  menuToggle.click(toggleMenu);
-  $('.menu a').click(toggleMenu);
+    function toggleMenu() {
+      menuToggle.toggleClass('active');
+      body.toggleClass('menu-open');
+    }
+
+    menuToggle.click(toggleMenu);
+    $('.menu a').click(toggleMenu);
+
+    $('.bio-opener-hayder').on('click', function() {
+      $('.bio-hayder').toggleClass('show');
+      $('body').toggleClass('people-bio-open');
+      menuToggle.toggle();
+    });
+    $('.bio-opener-anthanh').on('click', function() {
+      $('.bio-anthanh').toggleClass('show');
+      $('body').toggleClass('people-bio-open');
+      menuToggle.toggle();
+    });
+    $('.people-bio .bio-closer').on('click', function() {
+      $('.people-bio .bio-content').addClass('out');
+      $('.people-bio .bio-image').addClass('out');
+      $('body').removeClass('people-bio-open');
+      setTimeout(function() {
+        menuToggle.toggle();
+        $('.people-bio').removeClass('show');
+        $('.bio-image').removeClass('out');
+        $('.bio-content').removeClass('out');
+      }, 1500);
+    });
+
+    var rellax = new Rellax('.rellax');
+
+    var sr = ScrollReveal();
+    sr.reveal('.reveal', {
+      easing: 'ease-in-out'
+    });
+
+    var waypoint = new Waypoint({
+      element: $('.people')[0],
+      handler: function(direction) {
+        switch (direction) {
+          case 'down':
+            $('.contact-row-main').css('z-index', 0);
+            break;
+          case 'up':
+            $('.contact-row-main').css('z-index', -1);
+            break;
+        }
+      }
+    });
+
+  });
 
 })();
-
-$(document).ready(function() {
-  $('.bio-opener-hayder').on('click', function() {
-   $('.bio-hayder').toggleClass('show');
-   $('body').toggleClass('people-bio-open');
-  });
-  $('.bio-opener-anthanh').on('click', function() {
-   $('.bio-anthanh').toggleClass('show');
-   $('body').toggleClass('people-bio-open');
-  });
-  $('.people-bio .bio-closer').on('click', function() {
-   $('.people-bio .bio-content').addClass('out');
-   $('.people-bio .bio-image').addClass('out');
-   $('body').removeClass('people-bio-open');
-   setTimeout(function(){
-    $('.people-bio').removeClass('show');
-    $('.bio-image').removeClass('out');
-    $('.bio-content').removeClass('out');
-  },1500);
-  });
-});
