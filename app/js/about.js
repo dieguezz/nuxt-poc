b3c25f148a0900e6dcc1c1e6b216058146e7f4c5
Hayder <hayder@etereo.io>
2017-03-12 13:48:06 +0100

    About page updates

--- app/js/about.js
+++ app/js/about.js
@@ -25,5 +25,46 @@ $(document).ready(function() {
         arrows: false,
         variableWidth: true
     });
+});
 
+
+$(window).scroll(function() {
+  var scrollTop = $(this).scrollTop();
+
+  $('.about-intro-cover').css({
+    opacity: function() {
+      var elementHeight = $(this).height(),
+          opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) + 0.75;
+      return opacity;
+    }
+  });
+
+  $('#about-intro h2').css({
+    opacity: function() {
+      var elementHeight = $(this).height(),
+          opacity = ((2 + (elementHeight - scrollTop) / elementHeight) * 1) - 1;
+      return opacity;
+    }
+  });
+
+  $('#about-intro h4').css({
+    opacity: function() {
+      var elementHeight = $(this).height(),
+          opacity = ((4 + (elementHeight - scrollTop) / elementHeight) * 1) - 1;
+      return opacity;
+    }
+  });
+
+  $('#about-intro .scrolldown').css({
+    opacity: function() {
+      var elementHeight = $(this).height(),
+          opacity = ((4 + (elementHeight - scrollTop) / elementHeight) * 1) - 1;
+      return opacity;
+    }
+  });
+});
+
+$('.about-navigation li').click(function() {
+    $('.about-navigation li.active').removeClass('active');
+    $(this).addClass('active');
 });
