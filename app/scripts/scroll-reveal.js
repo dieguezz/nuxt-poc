31f9dc981d0cce08a499211ef144e1d3c8f64f48
Anthanh Pham Trinh <antai.pt@gmail.com>
2017-04-09 16:26:33 +0200

    SR refactor

---
+++ app/scripts/scroll-reveal.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  var sr = ScrollReveal();
+  sr.reveal('h3,h4,h5,p,section a', {
+    origin: 'bottom',
+    scale: 1,
+    reset: true,
+    distance: '50px',
+    duration: 750,
+    delay: 350
+  });
+
+  sr.reveal('#partners ul li', {
+    scale: 1,
+    reset: true,
+    distance: '50px',
+    duration: 1750
+  }, 150);
+
+});
