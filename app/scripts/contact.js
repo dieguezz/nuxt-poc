87882ab8fd3b5c5a29eb4aad5ab3fecbe3ce509b
Anthanh Pham Trinh <antai.pt@gmail.com>
2017-04-11 19:29:49 +0200

    Contact form with formspree integration

--- app/scripts/contact.js
+++ app/scripts/contact.js
@@ -14,24 +14,20 @@ $(document).ready(function() {
 
     $(this).addClass('sending');
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'services/email.php', true);
-
-    //Send the proper header information along with the request
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-    xhr.onreadystatechange = function() { //Call a function when the state changes.
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        setTimeout(function() {
-          $(this).removeClass('sending');
-        }, 2000);
-      }
-    };
-    xhr.send($.param({
-      name: name,
-      email: email,
-      message: message
-    }));
+    $.ajax({
+      url: 'https://formspree.io/hello@etereo.io',
+      method: 'POST',
+      data: {
+        '_subject': name,
+        email: email,
+        message: message
+      },
+      dataType: 'json'
+    }).then(function() {
+      setTimeout(function() {
+        $(this).removeClass('sending');
+      }, 2000);
+    });
 
   });
 });
