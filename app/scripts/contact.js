b58f3fb21559296bbbd61e0c168d315ee50897b0
Anthanh Pham Trinh <antai.pt@gmail.com>
2017-04-11 18:49:35 +0200

    contact form

--- app/scripts/contact.js
+++ app/scripts/contact.js
@@ -1,8 +1,39 @@
 'use strict';
 
-/* Form Sending */
+/* Sending */
 $(document).ready(function() {
-  $('.sender').on('click', function() {
-    $(this).toggleClass('sending');
+  $('.sender').on('click', function(event) {
+    event.preventDefault();
+    var name = $('#contact #name').val();
+    var email = $('#contact #email').val();
+    var message = $('#contact #message').val();
+
+    if (!name || !email || !message) {
+      return;
+    }
+
+    $(this).addClass('sending');
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'services/email.php', true);
+
+    //Send the proper header information along with the request
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    xhr.onreadystatechange = function() { //Call a function when the state changes.
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        setTimeout(function() {
+          $(this).removeClass('sending');
+        }, 2000);
+      }
+    };
+    xhr.send($.param({
+      name: name,
+      email: email,
+      message: message
+    }));
+
+
+
   });
 });
