fb8df5f80d4ec51e731b66def69af211b6b09190
Hayder <hayder@etereo.io>
2017-04-16 14:57:45 +0200

    - General fixes. - Homepage option 2. (add ‘three’ class to body to compare).

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -24,3 +24,42 @@ $(document).ready(function() {
   }, time1);
 
 });
+
+
+/* HOME Option 3 */
+
+$(document).ready(function () {
+    var $horizontal = $('.central-navigation ul');
+
+    $(window).scroll(function () {
+        var s = $(this).scrollTop(),
+            d = $(document).height(),
+            c = $(this).height();
+
+        var $scrollPercent = (s / (d - c));
+
+        var position = ($scrollPercent * ($(document).width() - $horizontal.width()));
+
+        $horizontal.css({
+            'left': position
+        });
+    });
+});
+
+$(document).ready(function(){
+   $(window).scroll(function() {
+       if ($('body').height() <= ($(window).height() + $(window).scrollTop())) {
+           $('.scroll-down').css('opacity','0');
+           $('.claim-bottom').css('margin-bottom','-75px');
+       }
+       else {
+         $('.scroll-down').css('opacity','1');
+         $('.claim-bottom').css('margin-bottom','-25px');
+       }
+    });
+});
+
+$('.central-navigation ul li').hover(
+       function(){ $('.central-navigation ul li:first-child').removeClass('active') },
+       function(){ $('.central-navigation ul li:first-child').addClass('active') }
+)
