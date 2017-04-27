271e78599f762a752f64da784066a4ef04b3ca2b
Anthanh Pham Trinh <antai.pt@gmail.com>
2017-04-27 18:53:12 +0200

    Index cookies

--- app/scripts/cookies.js
+++ app/scripts/cookies.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
   if (!window.localStorage.getItem('cookies')) {
     setTimeout(function() {
-      $('#home, .contact-page').addClass('cookies-active');
+      $('#home, .contact-page, .home-page').addClass('cookies-active');
     }, 3000);
   }
 
